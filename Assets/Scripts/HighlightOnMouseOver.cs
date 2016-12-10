fd347df655cb42d3946e414949379cca8316ecd5
Brian Dean Jennings <brian@bdjnews.com>
2016-12-11 01:01:10 +1300

    Marionette controls work

---
+++ Assets/Scripts/HighlightOnMouseOver.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class HighlightOnMouseOver : MonoBehaviour {
+  private MeshRenderer m_Renderer;
+
+  public bool Highlight;
+  private bool m_LastHighlight;
+
+  private void Start() {
+    m_Renderer = GetComponent<MeshRenderer>();
+  }
+
+  private void Update() {
+    if (m_LastHighlight == Highlight) {
+      return;
+    }
+
+    m_LastHighlight = Highlight;
+    m_Renderer.material.shader = Shader.Find(Highlight ? "Self-Illumin/Outlined Diffuse" : "Diffuse");
+  }
+}
