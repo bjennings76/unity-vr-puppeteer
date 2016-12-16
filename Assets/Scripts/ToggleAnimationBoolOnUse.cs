dbedbcdafab5aa9c665bacfd34f66e7d7e32c3ef
Brian Dean Jennings <brian@bdjnews.com>
2016-12-16 21:43:36 +1300

    Working curtains

---
+++ Assets/Scripts/ToggleAnimationBoolOnUse.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using VRTK;
+
+[RequireComponent(typeof(VRTK_InteractableObject))]
+public class ToggleAnimationBoolOnUse : MonoBehaviour {
+	[SerializeField] private Animator m_Animator;
+	[SerializeField] private string m_BoolName;
+
+	private VRTK_InteractableObject m_Interactable;
+
+	private void Start() {
+		m_Interactable = GetComponent<VRTK_InteractableObject>();
+		if (m_Interactable) { m_Interactable.InteractableObjectUsed += OnUsed; }
+	}
+
+	private void OnUsed(object sender, InteractableObjectEventArgs e) {
+		if (m_Animator) {
+			var b = m_Animator.GetBool(m_BoolName);
+			m_Animator.SetBool(m_BoolName, !b);
+		}
+	}
+}
