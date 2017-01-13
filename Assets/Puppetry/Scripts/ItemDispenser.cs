1f37c19a29986a64ecaa55f601825d0cd60a37b3
Brian Dean Jennings <brian@bdjnews.com>
2017-01-14 01:04:58 +1300

    feat(Spinner): Characters now change appropriately as carousel spins.

--- Assets/Puppetry/Scripts/ItemDispenser.cs
+++ Assets/Puppetry/Scripts/ItemDispenser.cs
@@ -1,34 +1,57 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using Utils;
 
 public class ItemDispenser : MonoBehaviour {
 	[SerializeField] private GameObject[] m_ItemPrefabs;
 	[SerializeField] private ItemSlot[] m_ItemSlots;
+	[SerializeField] private HingeJoint m_Hinge;
 
 	private List<IItemCreator> m_ItemList;
+	private float m_LastAngle;
+	private int m_Cursor;
+
+	private int SlotCount {  get { return m_ItemSlots == null || m_ItemSlots.Length == 0 ? 0 : m_ItemSlots.Length; } }
+	private int ItemCount { get { return m_ItemList == null || m_ItemList.Count == 0 ? 0 : m_ItemList.Count; } }
 
 	private int Cursor {
 		get { return m_Cursor; }
 		set {
 			m_Cursor = value;
-			m_Cursor = Mathf.Clamp(m_Cursor, 0, m_ItemList == null ? 0 : m_ItemList.Count - 1);
+			m_Cursor = ItemCount > 0 ? MathUtils.Mod(m_Cursor, ItemCount) : 0;
 		}
 	}
-	private int m_Cursor;
+
+	private int BackCursor { get { return ItemCount > 0 ? MathUtils.Mod(Cursor - SlotCount, ItemCount) : 0; } }
 
 	private void Start() {
 		m_ItemSlots = GetComponentsInChildren<ItemSlot>();
 		PopulateItemList();
-		UpdateItemSlots();
+		PopulateItemSlots();
 	}
 
-	private void UpdateItemSlots() {
-		m_ItemSlots.ForEach(slot => {
-			slot.Spawn(m_ItemList[Cursor]);
-			Cursor++;
-		});
+	private void Update() {
+		var lastAngle = m_LastAngle;
+		var currentAngle = m_LastAngle = m_Hinge.angle;
+		var currentSegment = GetSegment(currentAngle);
+		var lastSegment = GetSegment(lastAngle);
+
+		if (currentSegment == lastSegment) return;
+
+
+		var forward = Mathf.DeltaAngle(lastAngle, currentAngle) > 0;
+		Cursor = forward ? Cursor + 1 : Cursor - 1;
+		var slotIndex = MathUtils.Mod(forward ? SlotCount - currentSegment : SlotCount - currentSegment - 1, SlotCount);
+		var itemIndex = forward ? Cursor : BackCursor;
+
+		XDebug.Log(this, "Segment {0} --> {1}. Spawning Item #{2} in slot {3}", lastSegment, currentSegment, itemIndex, slotIndex);
+		m_ItemSlots[slotIndex].Spawn(m_ItemList[itemIndex]);
+	}
+
+	private int GetSegment(float hingeAngle) {
+		var segmentAngle = 360f / SlotCount;
+		var segment = MathUtils.Mod(Mathf.FloorToInt(hingeAngle / segmentAngle), SlotCount);
+		return segment;
 	}
 
 	private void PopulateItemList() {
@@ -41,5 +64,12 @@ public class ItemDispenser : MonoBehaviour {
 		}
 
 		m_ItemList = list;
+	}
+
+	private void PopulateItemSlots() {
+		m_ItemSlots.ForEach(slot => {
+			slot.Spawn(m_ItemList[Cursor]);
+			Cursor++;
+		});
 	}
 }
