6f2b0759ba6fec5ca8797f5bd5c3d47a33c52c28
Brian Dean Jennings <brian@bdjnews.com>
2017-01-25 01:04:10 +1300

    feat(Theatre): Added moveable/usable spotlights

---
+++ Assets/Camera/Scripts/InteractableSpotlight.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Utils;
+using VRTK;
+
+public class InteractableSpotlight : VRTK_InteractableObject {
+	private Light[] m_Lights;
+
+	private bool m_Using;
+
+	private void Start() { m_Lights = GetComponentsInChildren<Light>(); }
+
+	public override void StartUsing(GameObject previousUsingObject) {
+		base.StartUsing(previousUsingObject);
+		m_Lights.ForEach(l => l.enabled = !l.enabled);
+	}
+
+	public override void StopUsing(GameObject previousUsingObject) {
+		base.StopUsing(previousUsingObject);
+		m_Lights.ForEach(l => l.enabled = !l.enabled);
+	}
+}
