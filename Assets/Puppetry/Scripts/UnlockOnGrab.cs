8b09b642a8c2446d47380547beba5018d4da5450
Brian Dean Jennings <brian@bdjnews.com>
2017-01-26 01:23:24 +1300

    feat(Character): Added support for throwing puppets based on release velocity

--- Assets/Puppetry/Scripts/UnlockOnGrab.cs
+++ Assets/Puppetry/Scripts/UnlockOnGrab.cs
@@ -4,8 +4,21 @@ using VRTK;
 
 public class UnlockOnGrab : MonoBehaviour {
 	[SerializeField] private Rigidbody m_ObjectToUnlock;
+	[SerializeField] private float m_StayUnlockedVelocity = 5f;
+
+	private Transform m_InteractingObject;
+	private Vector3 m_LastInteractingObjectPosition;
+
+	private float Velocity { get { return m_InteractingObject ? (m_InteractingObject.position - m_LastInteractingObjectPosition).magnitude / Time.deltaTime : 0; } }
 
 	private void Start() { GetComponentsInChildren<VRTK_InteractableObject>().ForEach(RegisterInteractable); }
+
+	private void Update() {
+		if (m_InteractingObject) {
+			XDebug.Log(this, m_InteractingObject.name + " velocity: " + Velocity);
+			m_LastInteractingObjectPosition = m_InteractingObject.position;
+		}
+	}
 
 	private void RegisterInteractable(VRTK_InteractableObject interactable) {
 		if (interactable.GetComponentInParent<SkipUnlockOnGrab>()) return;
@@ -13,7 +26,15 @@ public class UnlockOnGrab : MonoBehaviour {
 		interactable.InteractableObjectUngrabbed += OnUngrabbed;
 	}
 
-	private void OnGrabbed(object sender, InteractableObjectEventArgs e) { m_ObjectToUnlock.isKinematic = false; }
+	private void OnGrabbed(object sender, InteractableObjectEventArgs e) {
+		m_ObjectToUnlock.isKinematic = false;
+		if (!e.interactingObject) return;
+		m_InteractingObject = e.interactingObject.transform;
+		m_LastInteractingObjectPosition = m_InteractingObject.position;
+	}
 
-	private void OnUngrabbed(object sender, InteractableObjectEventArgs e) { m_ObjectToUnlock.isKinematic = true; }
+	private void OnUngrabbed(object sender, InteractableObjectEventArgs e) {
+		if (Velocity < m_StayUnlockedVelocity) m_ObjectToUnlock.isKinematic = true;
+		m_InteractingObject = null;
+	}
 }
