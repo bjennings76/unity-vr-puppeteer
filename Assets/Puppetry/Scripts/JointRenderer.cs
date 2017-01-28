6f833b290b8f6a4f306c5a02576469b1253493ff
Brian Dean Jennings <brian@bdjnews.com>
2017-01-28 16:09:42 +1300

    feat(Puppets): Added strings to puppet controls

---
+++ Assets/Puppetry/Scripts/JointRenderer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using Utils;
+
+[ExecuteInEditMode, RequireComponent(typeof(LineRenderer))]
+public class JointRenderer : MonoBehaviour {
+	[SerializeField] private float m_Width = 0.005f;
+	[SerializeField] private Material m_Material;
+
+	private Joint m_Joint;
+	private LineRenderer m_Line;
+
+	private void Start() {
+		m_Line = this.GetOrAddComponent<LineRenderer>();
+
+		if (!m_Line.sharedMaterial) {
+			m_Line.sharedMaterial = m_Material;
+			m_Line.widthMultiplier = m_Width;
+		}
+
+		m_Joint = GetComponent<Joint>();
+	}
+
+	private void Update() {
+		if (!m_Joint) return;
+		var sourceAnchor = transform.TransformPoint(m_Joint.anchor);
+		var targetAnchor = m_Joint.connectedBody.transform.TransformPoint(m_Joint.connectedAnchor);
+		m_Line.SetPositions(new[] {sourceAnchor, targetAnchor});
+	}
+}
