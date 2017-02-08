b02ef49684eabe8947b24d88d3b0973ae737dc18
Brian Dean Jennings <brian@bdjnews.com>
2017-02-09 00:43:12 +1300

    feat(Props): Created a trash can for tossing unwanted props away.

--- Assets/Plugins/AssetBundleManager/AssetBundleManager.cs
+++ Assets/Plugins/AssetBundleManager/AssetBundleManager.cs
@@ -94,30 +94,34 @@ namespace AssetBundles
 				Debug.Log("[AssetBundleManager] " + text);
 		}
 	
-	#if UNITY_EDITOR
 		// Flag to indicate if we want to simulate assetBundles in Editor without building them actually.
 		public static bool SimulateAssetBundleInEditor 
 		{
 			get
 			{
+#if UNITY_EDITOR
 				if (m_SimulateAssetBundleInEditor == -1)
 					m_SimulateAssetBundleInEditor = EditorPrefs.GetBool(kSimulateAssetBundles, true) ? 1 : 0;
 				
 				return m_SimulateAssetBundleInEditor != 0;
+#else
+				return false;
+#endif
 			}
 			set
 			{
+#if UNITY_EDITOR
 				int newValue = value ? 1 : 0;
 				if (newValue != m_SimulateAssetBundleInEditor)
 				{
 					m_SimulateAssetBundleInEditor = newValue;
 					EditorPrefs.SetBool(kSimulateAssetBundles, value);
 				}
+#endif
 			}
 		}
 		
 	
-		#endif
 	
 		private static string GetStreamingAssetsPath()
 		{
---
+++ Assets/Puppetry/Scripts/TrashCan.cs
@@ -0,0 +1,10 @@
+﻿using Utils;
+using VRTK;
+
+public class TrashCan : VRTK_SnapDropZone {
+	public override void OnObjectSnappedToDropZone(SnapDropZoneEventArgs e) {
+		base.OnObjectSnappedToDropZone(e);
+		ForceUnsnap();
+		UnityUtils.Destroy(e.snappedObject);
+	}
+}
