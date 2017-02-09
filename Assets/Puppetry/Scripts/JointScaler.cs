2d2659262e035aa7d64d53318115ea8cfe1ab9bd
Brian Dean Jennings <brian@bdjnews.com>
2017-02-10 00:49:20 +1300

    feat(Characters): An attempt to fix the joint scaling issue to no avail

---
+++ Assets/Puppetry/Scripts/JointScaler.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+using Utils;
+
+public class JointScaler : MonoBehaviour {
+	public ScalableJoint[] Joints;
+
+	private void Start() { Joints = GetComponentsInChildren<Joint>().Select(j => new ScalableJoint(j)).ToArray(); }
+
+	private void FixedUpdate() { Joints.ForEach(j => j.FixedUpdate()); }
+
+	[Serializable]
+	public class ScalableJoint {
+		private readonly Joint m_Joint;
+		private readonly Vector3 m_InitialAnchor;
+		private readonly Vector3 m_InitialConnectedAnchor;
+		private readonly float m_InitialScale;
+
+		private float m_LastScale;
+
+		private float Ratio { get { return m_LastScale / m_InitialScale; } }
+
+		public ScalableJoint(Joint joint) {
+			m_Joint = joint;
+			m_InitialAnchor = joint.anchor;
+			m_InitialConnectedAnchor = joint.connectedAnchor;
+			m_InitialScale = joint.transform.lossyScale.x;
+		}
+
+		public void FixedUpdate() {
+			if (m_LastScale.Approximately(m_Joint.transform.lossyScale.x)) return;
+			m_LastScale = m_Joint.transform.lossyScale.x;
+			m_Joint.anchor = m_InitialAnchor * Ratio;
+			m_Joint.connectedAnchor = m_InitialConnectedAnchor * Ratio;
+		}
+	}
+}
