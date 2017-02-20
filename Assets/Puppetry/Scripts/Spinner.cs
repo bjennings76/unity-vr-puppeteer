018f4c8494e95d822a05ce852f9c048dda088d39
Brian Dean Jennings <brian@bdjnews.com>
2017-02-21 01:28:40 +1300

    feat(Spinner): Smoothed out interactions with spinner

--- Assets/Puppetry/Scripts/Spinner.cs
+++ Assets/Puppetry/Scripts/Spinner.cs
@@ -1,12 +1,40 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using UnityEngine;
 
 namespace Utils {
+	public class AverageTracker {
+		public int MaxCount = 3;
+
+		public float Value { get; private set; }
+
+		private readonly List<float> m_List = new List<float>();
+
+		private int m_Index;
+
+		public void Add(float num) {
+			if (m_List.Count < MaxCount) { m_List.Add(num); }
+			else {
+				m_List[m_Index] = num;
+				m_Index = (m_Index + 1) % MaxCount;
+			}
+			Value = m_List.Average();
+		}
+
+		public void Reset() {
+			m_List.Clear();
+			m_Index = 0;
+		}
+	}
+
 	[RequireComponent(typeof(TriggerCollisionTracker))]
 	public class Spinner : MonoBehaviour {
+		[SerializeField] private float m_MaxSpeed = 10f;
 		[SerializeField] private float m_Push = 1.2f;
 		[SerializeField] private Rigidbody m_Rigidbody;
+		[SerializeField] private Collider m_Collider;
+		[SerializeField, Range(2, 10)] private int m_FrameAverageCount = 3;
 
 		private readonly List<Transform> m_Targets = new List<Transform>();
 		private float m_LastRotation;
@@ -20,8 +48,9 @@ namespace Utils {
 		private Transform m_Transform;
 		private float m_RotationSpeed;
 		private float m_LastRotationTime;
-		private bool m_WasTracking;
 		private float m_Angle;
+
+		private readonly AverageTracker m_FrameAverage = new AverageTracker();
 
 		public float Angle { get { return m_Angle; } private set { m_Angle = MathUtils.ClampAngle(value); } }
 
@@ -33,37 +62,20 @@ namespace Utils {
 			m_Tracker = GetComponent<TriggerCollisionTracker>();
 			m_Tracker.OnEnter += OnEnter;
 			m_Tracker.OnExit += OnExit;
+			m_FrameAverage.MaxCount = m_FrameAverageCount;
 		}
 
 		private void LateUpdate() {
-			CheckTarget();
-			CheckRotate();
+			if (Target) UpdateTarget();
+			else UpdateRotation();
 		}
 
-		private void CheckRotate() {
-			if (Target) return;
-
-			if (m_WasTracking) {
-				m_WasTracking = false;
-				this.DOKill();
-				var duration = Mathf.Clamp(m_RotationSpeed / (m_Rigidbody.angularDrag > 0 ? m_Rigidbody.angularDrag : 1), 0, 5);
-				DOTween.To(() => m_RotationSpeed, s => m_RotationSpeed = s, 0f, duration).SetTarget(this).SetEase(Ease.OutQuad);
-			}
-
-			LogRotation();
+		private void UpdateRotation() {
 			m_Transform.Rotate(m_Axis, m_RotationSpeed, Space.World);
 			Angle += m_RotationSpeed;
 		}
 
-		private void LogRotation() {
-			m_RotationDelta = m_Transform.rotation.eulerAngles.y - m_LastRotation;
-			m_RotationTimeDelta = Time.time - m_LastRotationTime;
-			m_LastRotation = m_Transform.rotation.eulerAngles.y;
-			m_LastRotationTime = Time.time;
-		}
-
-		private void CheckTarget() {
-			if (!Target) return;
+		private void UpdateTarget() {
 			m_LookPos = ProjectPointOnPlane(m_Axis, m_Transform.position, Target.position);
 			var lastLook = m_LookDir;
 			m_LookDir = m_LookPos - m_Transform.position;
@@ -71,12 +83,14 @@ namespace Utils {
 			m_Transform.Rotate(m_Axis, rotationAmount, Space.World);
 			Angle += rotationAmount;
 
-			LogRotation();
-			m_RotationSpeed = m_RotationDelta * m_Push * m_RotationTimeDelta * 100;
-			m_WasTracking = true;
+			m_FrameAverage.Add(Mathf.Clamp((m_Transform.rotation.eulerAngles.y - m_LastRotation) * m_Push, -m_MaxSpeed, m_MaxSpeed));
+			m_RotationDelta = m_FrameAverage.Value;
+			m_RotationTimeDelta = Time.time - m_LastRotationTime;
+			m_LastRotation = m_Transform.rotation.eulerAngles.y;
+			m_LastRotationTime = Time.time;
+			m_RotationSpeed = m_RotationDelta * m_RotationTimeDelta * 100;
+			XDebug.Log("Δ: " + m_RotationDelta.ToString("N2"));
 		}
-
-		private void FixedUpdate() { }
 
 		private void OnEnter(Collider other) {
 			m_Targets.Add(other.transform);
@@ -88,11 +102,23 @@ namespace Utils {
 		}
 
 		private void OnExit(Collider other) {
-			var wasTarget = other.transform == Target;
+			var lastTarget = Target;
 			m_Targets.Remove(other.transform);
 
-			if (!Target) m_Rigidbody.AddTorque(0, m_RotationDelta * m_Push * m_RotationTimeDelta * 100, 0, ForceMode.VelocityChange);
-			else if (wasTarget) InitNewTarget();
+			if (!Target) StartRotation();
+			else if (lastTarget != Target) InitNewTarget();
+		}
+
+		private void StartRotation() {
+			this.DOKill();
+			if (m_Collider) {
+				m_Collider.enabled = false;
+				UnityUtils.DelayAction(0.05f, () => m_Collider.enabled = true, this);
+			}
+			m_FrameAverage.Reset();
+			XDebug.Log("Release at Δ " + m_RotationSpeed.ToString("N2"));
+			var duration = Mathf.Clamp(Mathf.Abs(m_RotationSpeed) / (m_Rigidbody.angularDrag > 0 ? m_Rigidbody.angularDrag : 1), 0, 5);
+			DOTween.To(() => m_RotationSpeed, s => m_RotationSpeed = s, 0f, duration).SetTarget(this).SetEase(Ease.OutQuad);
 		}
 
 		private void InitNewTarget() {
