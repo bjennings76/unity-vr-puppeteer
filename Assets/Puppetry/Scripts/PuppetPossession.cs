c1a3b53876f1cc14f38d9656498d6b99844d5983
Brian Dean Jennings <brian@bdjnews.com>
2017-02-22 00:35:05 +1300

    feat(Puppets): Started work on a 'Puppet Possession' mode

---
+++ Assets/Puppetry/Scripts/PuppetPossession.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using Utils;
+using VRTK;
+
+public class PuppetPossession : MonoBehaviour {
+	private VRTK_InteractableObject[] m_Interactables;
+
+	private void Start() {
+		m_Interactables = GetComponentsInChildren<VRTK_InteractableObject>();
+		m_Interactables.ForEach(i => i.InteractableObjectTouched += OnTouch);
+	}
+
+	private void OnTouch(object sender, InteractableObjectEventArgs e) {
+		XDebug.Log(this, "{0} is interacting with {1}", sender, e.interactingObject.name);
+	}
+}
