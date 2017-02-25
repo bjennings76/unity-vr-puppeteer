8def571469da2a7731add2de3c5e07198611f046
Brian Dean Jennings <brian@bdjnews.com>
2017-02-26 00:48:54 +1300

    feat(Props): Created system for adding in backdrops based on a root backdrop location

---
+++ Assets/Puppetry/Scripts/BackdropProp.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using Utils;
+
+[ExecuteInEditMode]
+public class BackdropProp : Prop {
+	[SerializeField, ReadOnly] private BackdropRoot m_Root;
+	[SerializeField, ReadOnly] private Vector3 m_PositionOffset;
+	[SerializeField, ReadOnly] private Quaternion m_RotationOffset;
+
+	private Vector3 m_LastRootPosition;
+	private Quaternion m_LastRootRotation;
+	private Vector3 m_LastBackgroundPosition;
+	private Quaternion m_LastBackgroundRotation;
+
+	private BackdropRoot Root { get { return m_Root ? m_Root : (m_Root = FindObjectOfType<BackdropRoot>()); } }
+
+	private void Start() {
+		if (!Root) return;
+
+		Root.SetBackdrop(this);
+		MatchRoot();
+	}
+
+	private void Update() {
+		if (!Root) return;
+		TrackRootMovement();
+		TrackPropMovement();
+	}
+
+	private void MatchRoot() {
+		if (!Root) return;
+		transform.position = Root.transform.TransformPoint(m_PositionOffset);
+		transform.rotation = Root.transform.rotation * m_RotationOffset;
+	}
+
+	private void TrackPropMovement() {
+		if (transform.position.Approximately(m_LastBackgroundPosition) && transform.rotation.Approximately(m_LastBackgroundRotation)) return;
+
+		m_LastBackgroundPosition = transform.position;
+		m_PositionOffset = Root.transform.InverseTransformPoint(m_LastBackgroundPosition);
+
+		m_LastBackgroundRotation = transform.rotation;
+		m_RotationOffset = Quaternion.Inverse(m_LastRootRotation) * m_LastBackgroundRotation;
+	}
+
+	private void TrackRootMovement() {
+		if (Root.transform.position.Approximately(m_LastRootPosition) && Root.transform.rotation.Approximately(m_LastRootRotation)) return;
+
+		m_LastRootPosition = Root.transform.position;
+		m_LastRootRotation = Root.transform.rotation;
+		MatchRoot();
+	}
+}
---
+++ Assets/Puppetry/Scripts/BackdropRoot.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using Utils;
+
+public class BackdropRoot : Singleton<BackdropRoot> {
+	[SerializeField, ReadOnly] private BackdropProp m_CurrentBackdrop;
+
+	public void SetBackdrop(BackdropProp backdrop) {
+		if (m_CurrentBackdrop) UnityUtils.Destroy(m_CurrentBackdrop.gameObject);
+		m_CurrentBackdrop = backdrop;
+	}
+}
