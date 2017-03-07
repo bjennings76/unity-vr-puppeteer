500fbdb48df4e979342283c3eca5ed1546f625d3
Brian Dean Jennings <brian@bdjnews.com>
2017-03-08 00:41:52 +1300

    fix(Props): Cleaned up `UnlockOnThrow` so it doesn't include some test code.

--- Assets/Puppetry/Scripts/UnlockOnThrow.cs
+++ Assets/Puppetry/Scripts/UnlockOnThrow.cs
@@ -18,9 +18,7 @@ public class UnlockOnThrow : MonoBehaviour {
 	private void OnGrabbed(object sender, InteractableObjectEventArgs e) { m_Rigidbody.isKinematic = false; }
 
 	private void OnUngrabbed(object sender, InteractableObjectEventArgs e) {
-		var locked = m_Rigidbody.velocity.magnitude < m_StayUnlockedVelocity && !m_InteractableObject.IsInSnapDropZone();
-		var interactingObjets = m_InteractableObject.GetTouchingObjects();
-		XDebug.Log(this, "Touching objects = " + interactingObjets.AggregateString());
+		var locked = m_Rigidbody.velocity.magnitude < m_StayUnlockedVelocity;
 		XDebug.Log(this, "{0} release velocity: {1:N2} ({2})", e.interactingObject ? e.interactingObject.name : "unknown", m_Rigidbody.velocity.magnitude, locked ? "Locked" : "Ragdoll");
 		m_Rigidbody.isKinematic = locked;
 	}
